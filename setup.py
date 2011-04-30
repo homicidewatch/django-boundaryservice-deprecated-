46132b9854a52bcd94d7c9ad142f79ed3d14b657
Chris Amico <eyeseast@gmail.com>
2011-04-30 15:12:44 -0400

    A little more in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(
     name = "django-boundaryservice",
     description = "A reusible boundaries API for GeoDjango",
+    long_description = open('README.md').read(),
     version = "0.1.0",
     packages = [
         'boundaryservice',
